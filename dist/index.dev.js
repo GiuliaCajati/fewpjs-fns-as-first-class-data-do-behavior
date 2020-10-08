24c424732225ad4277b83844768958c0b6698ae3
giuliacajati <giuliacajati@gmail.com>
2020-10-08 08:10:03 -0400

    Done.

---
+++ dist/index.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/* Given Code, don't edit */
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(e) {
+  //timeString =  empty text box value 
+  var timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString)); //uses function below taking in the inout from above 
+}
+/* Write your implementation of displayMessage() */
+
+
+function displayMessage(msg) {
+  //filling empty h1 (under text box)
+  document.getElementById("greeting").innerText = msg;
+}
+
+function greet(timeStr) {
+  var hour = parseInt(timeStr, 10);
+  if (hour < 12) return "Good Morning";
+  if (hour > 17) return "Good Evening";
+  return "Good Afternoon";
+}
--- index.js
+++ index.js
@@ -3,11 +3,22 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    //timeString =  empty text box value 
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
+        //uses function below taking in the inout from above 
 }
 
-/* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+    //filling empty h1 (under text box)
+    document.getElementById("greeting").innerText = msg;
+}
+
+function greet(timeStr) {
+    const hour = parseInt(timeStr, 10);
+    if (hour < 12) return "Good Morning"
+    if (hour > 17) return "Good Evening"
+    return "Good Afternoon"
+}
